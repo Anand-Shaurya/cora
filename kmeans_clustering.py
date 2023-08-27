d92ee2a76f4290e15f7365dfbb3cba003ddcc758
ANAND SHAURYA <shaurya252anand@gmail.com>
2023-08-27 21:05:23 +0530

    Update kmeans_clustering.py

--- kmeans_clustering.py
+++ kmeans_clustering.py
@@ -1,14 +1,1 @@
-from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_score
 
-# Load preprocessed data
-from preprocess import X_tfidf, y_encoded
-
-# Apply k-means clustering
-num_clusters = 7  # Number of clusters
-kmeans = KMeans(n_clusters=num_clusters, random_state=42)
-cluster_labels = kmeans.fit_predict(X_tfidf)
-
-# Evaluate clustering
-silhouette_avg = silhouette_score(X_tfidf, cluster_labels)
-print(f"Silhouette Score: {silhouette_avg:.4f}")
